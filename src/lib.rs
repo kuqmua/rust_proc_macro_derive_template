2f9532c2ace0484c1e23db0dde2c60e92067b0aa
Sergey Shmakov <31436321+kuqmua@users.noreply.github.com>
2023-05-17 11:17:48 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
 #[proc_macro_derive()]
 pub fn (input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    //todo - use panic_location function from https://github.com/kuqmua/proc_macro_helpers instead of this std::panic::set_hook (reusage)
-    std::panic::set_hook(Box::new(move |panic_info| {
-        if let Some(location) = panic_info.location() {
-            eprintln!("{proc_macro_name} panic occurred in {}:{}:{}", location.file(), location.line(), location.column());
-        } else {
-            eprintln!("{proc_macro_name} panic occurred but can't get location information...");
-        }
-    }));
     let proc_macro_name = "";
+    proc_macro_helpers::panic_location::panic_location("");//panic_location function from https://github.com/kuqmua/proc_macro_helpers
     let ast: syn::DeriveInput =
         syn::parse(input).unwrap_or_else(|_| panic!("{proc_macro_name} let ast: syn::DeriveInput = syn::parse(input) failed"));
     let ident = &ast.ident;
